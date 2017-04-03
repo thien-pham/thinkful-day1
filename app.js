d0f0a1b36a3b2b1bf57fc53261f13412d4b8f807
Zach Williams <saumanahaii@gmail.com>
2017-04-03 11:50:43 -0400

    4

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
-function WhoAmI(name, age){
-  let str= `Hi, my  name is ${name} ${age}`;
-  console.log(str);
+function WhoAmI(name, last,age){
+  let dOB = 2017-age;
+  console.log(`Hi, my  name is ${name} ${last}`);
+  console.log(`I was born in ${dOB}`)
 }
 
-WhoAmI("George", "Orwell");
+WhoAmI("George", "Orwell", 999);
