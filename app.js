b773a8319255f9dfafbd8a99b44fb378ab3e0ac5
Zach Williams <saumanahaii@gmail.com>
2017-04-03 11:40:19 -0400

    2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 
-let str= 'Hi, my name is Chris and I\'m 29 years old';
-console.log(str);
+function WhoAmI(){
+  let str= 'Hi, my name is Chris and I\'m 29 years old';
+  console.log(str);
+}
+
+WhoAmI();
