d51bd1292d752b7dfdf8b2e82137ccd4414aabe9
Zach Williams <saumanahaii@gmail.com>
2017-04-03 12:09:17 -0400

    5

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 
 function WhoAmI(name, last,age){
   console.log(`Hi, my  name is ${name} ${last}`);
-
-  console.log(yearOfBirth(12));
+  console.log(yearOfBirth(age));
 }
 
+
+
 function yearOfBirth(age){
+  try{
+    if(age<0){
+      throw new Error("Nums can't be negative.  Try again!");
+    }
+  }catch(err){
+    //console.log(`"the error is... ${e} `)
+    console.log(err.message);
+  }
   return 2017 - age;
 }
 
-WhoAmI("George", "Orwell", 100);
+WhoAmI("George", "Orwell", -100);
