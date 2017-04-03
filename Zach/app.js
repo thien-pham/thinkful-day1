ad0cd7563c03db53b06afd34f6093eb56b4865a6
Zach Williams <saumanahaii@gmail.com>
2017-04-03 14:08:19 -0400

    Moved try catch out of the function

--- Zach/app.js
+++ Zach/app.js
@@ -1,45 +1,41 @@
 
 function WhoAmI(name, last,age){
-  try{
-    if(name === typeof('undefined') ||
-      last === typeof('undefined') ||
-      typeof(name) !== 'string' ||
-      typeof(last) !== 'string' ||
-      typeof(age) !== 'number')
-      {
-        
-        throw new Error("Arguments not valid");
-    }
-    else{
-      console.log(`Hi, my  name is ${name} ${last}`);
-      console.log(yearOfBirth(age));
-    }
-  }catch(err){
-    console.log(err.message);
+  if(
+    typeof(name) !== 'string' ||
+    typeof(last) !== 'string' ||
+    typeof(age) !== 'number')
+    {
+      throw new Error("Arguments not valid, format is string, string, number.");
   }
-
-
-
+  else{
+    console.log(`Hi, my  name is ${name} ${last}`);
+    console.log(yearOfBirth(age));
+  }
 }
-
-
 
 function yearOfBirth(age){
-  try{
+
     if(age<0){
       throw new Error("Nums can't be negative.  Try again!");
+    }else{
+        return 2017 - age;
     }
-  }catch(err){
-    //console.log(`"the error is... ${e} `)
-    console.log(err.message);
-  }
-  return 2017 - age;
+
+
 }
-console.log("1");
-WhoAmI("sdf","sdff", 20);
-console.log("2");
-WhoAmI("asdf",20, 20);
-console.log("3");
-WhoAmI(20,"d",20);
-console.log("4");
-WhoAmI("20","d","test");
+
+//Test 1
+try{
+  console.log("1");
+  WhoAmI("sdf","sdff", -20);
+}catch(err){
+  console.log(err.message);
+}
+
+//test 2
+try{
+  console.log("2");
+  WhoAmI("asdf",20, 20);
+}catch(err){
+  console.log(err.message);
+}
